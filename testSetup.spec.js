381044e24ea100c7c8acef2e7ae35031e6e49795
Adam Brunner <brunner.adam@gmail.com>
2016-06-22 14:02:03 +0200

    fix(test): do not initialize chai before each test

--- testSetup.spec.js
+++ testSetup.spec.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var sinon = require('sinon');
+const sinon = require('sinon');
+const chai = require('chai');
+chai.use(require('sinon-chai'));
+chai.use(require('chai-generator'));
+chai.use(require('chai-is-generator'));
 
 before(function() {
-  var chai = require('chai');
-  chai.use(require('sinon-chai'));
-  chai.use(require('chai-generator'));
-  chai.use(require('chai-is-generator'));
-
   global.expect = chai.expect;
 
   sinon.stub.returnsWithResolve = function(data) {
@@ -17,7 +16,6 @@ before(function() {
   sinon.stub.returnsWithReject = function(error) {
     return this.returns(Promise.reject(error));
   };
-
 });
 
 
